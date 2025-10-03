f0e4fcf91120faac4cc02bb63c8e8e32334ad311
takku1220 <takutakutakumi8989@gmail.com>
2025-10-03 21:00:10 +0900

    Update 参照.py

--- 参照.py
+++ 参照.py
@@ -55,9 +55,10 @@ def check_food(text):
                 results.append(msg)
 
     if not results:
-        results.append("判定不能です。まず、ひらがな、カタカナ、漢字の3パターンを試し、この表示が尚出てくるのであれば、LINE、Slack等で連絡してください。[受付時間：8時～21時]")
+        results.append("判定不能です。Googleformの→https://forms.gle/8YMNuueEZqaEKAox8もしくはLINE、Slack等で連絡してください。")
 
     return results
+
 
 
 
